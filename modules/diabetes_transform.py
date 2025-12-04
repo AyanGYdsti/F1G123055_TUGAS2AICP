4a3a9e6a707d8e8587cd6308467c51183b071d41
AyanGYdsti <yangisna02.com>
2025-12-04 10:44:59 +0800

    TUGAS 2 AICP F1G123055

--- modules/diabetes_transform.py
+++ modules/diabetes_transform.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 import tensorflow_transform as tft
 
-# KUNCI KOMPATIBILITAS TFX 1.16:
-# Kita mendefinisikan key fitur secara eksplisit.
 DENSE_FLOAT_FEATURE_KEYS = ['BMI', 'DiabetesPedigreeFunction']
 DENSE_INT_FEATURE_KEYS = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'Age']
 LABEL_KEY = 'Outcome'
@@ -18,18 +16,11 @@ def preprocessing_fn(inputs):
     """
     outputs = {}
 
-    # 1. Normalisasi Z-Score
-    # Menggabungkan float dan int karena semuanya butuh dinormalisasi
     for key in DENSE_FLOAT_FEATURE_KEYS + DENSE_INT_FEATURE_KEYS:
-        # tft.scale_to_z_score adalah fungsi standar di TFX 1.x
         outputs[_transformed_name(key)] = tft.scale_to_z_score(inputs[key])
 
-    # 2. Bucketing Age (Umur)
-    # Mengubah umur menjadi kategori (0, 1, 2, 3, 4)
     outputs['Age_bucket'] = tft.bucketize(inputs['Age'], num_buckets=5)
 
-    # 3. Casting Label
-    # Mengubah label menjadi int64 agar kompatibel dengan Learner nanti
     outputs[LABEL_KEY] = tf.cast(inputs[LABEL_KEY], tf.int64)
 
     return outputs
